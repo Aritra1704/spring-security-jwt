57d8cfc14418174c7a11b7bcf444f3ca0b860581
Aritra1704 <aritra1704@gmail.com>
2021-05-14 18:16:20 +0530

    Resolved annotation issue

--- src/main/java/io/javabrains/springsecurityjwt/HelloResource.java
+++ src/main/java/io/javabrains/springsecurityjwt/HelloResource.java
@@ -15,8 +15,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloResource {
 
     @Autowired
